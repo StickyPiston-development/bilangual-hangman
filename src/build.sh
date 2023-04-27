736c4b843f933796911a035904831922ba1341b1
Sticky Piston <97804691+MrStickyPiston@users.noreply.github.com>
2023-04-27 20:21:04 +0200

    Removed icon from linux build

--- src/build.sh
+++ src/build.sh
@@ -1,1 +1,1 @@
-pyinstaller --noconfirm --onefile --windowed --icon "./assets/hangman.ico" --add-data="./assets:assets/"  "./main.py"
+pyinstaller --noconfirm --onefile --windowed --add-data="./assets:assets/"  "./main.py"
